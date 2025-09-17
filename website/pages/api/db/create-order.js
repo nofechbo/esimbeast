8b1b1c0aaee80522c89e7fd6605a4b5d6a3747fe
nofech.ben-or <nofechbo@gmail.com>
2025-09-17 23:53:42 +0300

    dev - api/db/create-order imlemented and tested

---
+++ website/pages/api/db/create-order.js
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        res.setHeader('Allow', ['POST'])
+        return res.status(405).end('Method Not Allowed');
+    }
+
+    try {
+        const {
+            orderId,
+            email,
+            productId,
+            productName,
+            countryCodes,
+            data,
+            duration,
+            price,
+        } = req.body
+
+        const newOrder = await prisma.planOrder.create({
+            data: {
+                orderId,
+                email,
+                productId,
+                productName,
+                countryCodes,
+                data,
+                duration,
+                price,
+                orderTime: new Date()
+            }
+        })
+
+        res.status(201).json(newOrder)
+
+    } catch (err) {
+        console.error('Error creating order:' , err)
+        res.status(500).json({ error: 'Failed to create order'})
+    }
+}
