970f684ffc52de32efb230270428b2474f1fbced
Eliran Haziza <eliranhaze@gmail.com>
2025-12-27 13:59:34 +0200

    added sitemap and robots

---
+++ website/pages/api/sitemap.xml.js
@@ -0,0 +1,88 @@
+import { getAllPlans } from "@/lib/db/plans";
+import slugify from "@/utils/formaters";
+
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://esimbeast.com";
+
+function generateSiteMap(plans) {
+  const today = new Date().toISOString().split("T")[0];
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <!-- Static Pages -->
+  <url>
+    <loc>${SITE_URL}</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/info/about-us</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/info/what-is-esim</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/info/supported-devices</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/info/affiliate-program</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/legal/terms-and-conditions</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>yearly</changefreq>
+    <priority>0.3</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/legal/privacy-policy</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>yearly</changefreq>
+    <priority>0.3</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/legal/cookies-policy</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>yearly</changefreq>
+    <priority>0.3</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/legal/refunds-policy</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>yearly</changefreq>
+    <priority>0.3</priority>
+  </url>
+  <!-- Dynamic Plan Pages -->
+${plans
+  .map(
+    (plan) => `  <url>
+    <loc>${SITE_URL}/plans/${slugify(plan.uniqueName)}</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>`
+  )
+  .join("\n")}
+</urlset>`;
+}
+
+export default async function handler(req, res) {
+  const plans = await getAllPlans();
+  const sitemap = generateSiteMap(plans);
+
+  res.setHeader("Content-Type", "text/xml");
+  res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
+  res.write(sitemap);
+  res.end();
+}
