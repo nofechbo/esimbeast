cbcbb36a7a8ec1ff634ffc096de8a36fb1a059eb
Eliran Haziza <eliranhaze@gmail.com>
2026-01-23 12:57:22 +0200

    added address

--- website/components/Footer.js
+++ website/components/Footer.js
@@ -15,56 +15,63 @@ export default function Footer() {
           />
         </div>
 
-        <div className={styles.linkSections}>
-          <div>
-            <h4 className={styles.sectionTitle}>ESIM</h4>
-            <ul className={styles.sectionList}>
-              <li>Popular</li>
-              <li>Countries</li>
-              <li>Regions</li>
-              <li>FAQ</li>
-            </ul>
+        <div className={styles.rightSection}>
+          <div className={styles.linkSections}>
+            <div>
+              <h4 className={styles.sectionTitle}>ESIM</h4>
+              <ul className={styles.sectionList}>
+                <li>Popular</li>
+                <li>Countries</li>
+                <li>Regions</li>
+                <li>FAQ</li>
+              </ul>
+            </div>
+
+            <div>
+              <h4 className={styles.sectionTitle}>ABOUT</h4>
+              <ul className={styles.sectionList}>
+                <li>
+                  <Link href="/info/about-us">About us</Link>
+                </li>
+                <li>
+                  <Link href="/info/what-is-esim">What is eSim</Link>
+                </li>
+                <li>
+                  <Link href="/info/supported-devices">Supported devices</Link>
+                </li>
+                <li>
+                  <Link href="/info/affiliate-program">Affiliate program</Link>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h4 className={styles.sectionTitle}>LEGAL</h4>
+              <ul className={styles.sectionList}>
+                <li>
+                  <Link href="/legal/terms-and-conditions">
+                    Terms & conditions
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/legal/privacy-policy">Privacy policy</Link>
+                </li>
+                <li>
+                  <Link href="/legal/cookies-policy">Cookies policy</Link>
+                </li>
+                <li>
+                  <Link href="/legal/refunds-policy">Refunds policy</Link>
+                </li>
+                <li>
+                  <Link href="/legal/fair-usage-policy">Fair usage policy</Link>
+                </li>
+              </ul>
+            </div>
           </div>
 
-          <div>
-            <h4 className={styles.sectionTitle}>ABOUT</h4>
-            <ul className={styles.sectionList}>
-              <li>
-                <Link href="/info/about-us">About us</Link>
-              </li>
-              <li>
-                <Link href="/info/what-is-esim">What is eSim</Link>
-              </li>
-              <li>
-                <Link href="/info/supported-devices">Supported devices</Link>
-              </li>
-              <li>
-                <Link href="/info/affiliate-program">Affiliate program</Link>
-              </li>
-            </ul>
-          </div>
-
-          <div>
-            <h4 className={styles.sectionTitle}>LEGAL</h4>
-            <ul className={styles.sectionList}>
-              <li>
-                <Link href="/legal/terms-and-conditions">
-                  Terms & conditions
-                </Link>
-              </li>
-              <li>
-                <Link href="/legal/privacy-policy">Privacy policy</Link>
-              </li>
-              <li>
-                <Link href="/legal/cookies-policy">Cookies policy</Link>
-              </li>
-              <li>
-                <Link href="/legal/refunds-policy">Refunds policy</Link>
-              </li>
-              <li>
-                <Link href="/legal/fair-usage-policy">Fair usage policy</Link>
-              </li>
-            </ul>
+          <div className={styles.addressBlock}>
+            <h4 className={styles.addressTitle}>Address</h4>
+            <p className={styles.companyAddress}>KHITAR LIMITED, 33 Wyndham Street, 22F, Hong Kong Island, Hong Kong</p>
           </div>
         </div>
       </div>
@@ -75,12 +82,12 @@ export default function Footer() {
       <div className={styles.bottomNote}>
         © {new Date().getFullYear()} {SITE_NAME}
         <span className={styles.legalLinks}>
-          <Link href="/legal/terms-and-conditions">
-            <span className={styles.legalLink}>Terms</span>
+          <Link href="/legal/terms-and-conditions" className={styles.legalLink}>
+            Terms
           </Link>
           <span> | </span>
-          <Link href="/legal/privacy-policy">
-            <span className={styles.legalLink}>Privacy</span>
+          <Link href="/legal/privacy-policy" className={styles.legalLink}>
+            Privacy
           </Link>
         </span>
       </div>
